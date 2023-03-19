7dbfaa993555fbc11c30fa7409a15d339a186027
freetimezp <kraftukrnet@ukr.net>
2023-03-19 21:35:27 +0200

    04. js-portfolio-10-3D. Navbar component. Show, hide main menu.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-import './App.css';
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <div className='realtive z-0 bg-primary'> 
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
 
-    </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
