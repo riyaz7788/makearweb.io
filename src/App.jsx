9d31d591d041920e1ce943261db0ec8eb41bb809
riyaz7788 <132046163+riyaz7788@users.noreply.github.com>
2023-07-03 18:27:32 +0530

    ar

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,9 @@ const App = () => {
         </div>
 
         <About />
-        <Experience />
+        {/* <Experience />
         <Tech />
-        <Works />
+        <Works /> */}
         <Feedbacks />
 
         <div className='relative z-0'>
